8ae56397fef92bb9e3707102c5b1b666fbb0593f
techbeck015413 <techbeck015413@gmail.com>
2021-01-03 02:49:00 +0800

    db_connect_test_php

---
+++ db_connect_test_php/db_connection.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+//改成你登入phpmyadmin帳號
+$user = 'root';
+//改成你登入phpmyadmin密碼
+$passwd = '';
+//資料庫名稱
+$database = 'kevin_test';
+//實例化mysqli(資料庫路徑, 登入帳號, 登入密碼, 資料庫)
+$connect = new mysqli($host, $user, $passwd, $database);
+ 
+if ($connect->connect_error) {
+    die("連線失敗: " . $connect->connect_error);
+}
+echo "連線成功";
+?>
